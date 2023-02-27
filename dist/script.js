df2a874d51dff4c3fb7735f8b911a9c99cf2c834
kaustav991 <kaustav.wlh@gmail.com>
2023-02-27 18:51:15 +0530

    Revert "commit 27-02-2023"
    This reverts commit f232aae0b5a2151233c43122bfa01f486e93fbcb.

--- dist/script.js
+++ dist/script.js
@@ -1,51 +1,62 @@
-(function ($) {
-  var slickWrap = $(".slick-wrap");
-  slickWrap.slick({
-    dots: false,
-    autoplay: false,
-    //focusOnSelect: true,
-    infinite: true,
-    arrows: false,
-    speed: 300,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    vertical: true,
-    //asNavFor: '.slidetxt',
-  });
+(function($) {
 
-  var slideTxt = $(".slidetxt");
-  slideTxt.slick({
-    dots: false,
-    focusOnSelect: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    vertical: true,
-    infinite: false,
-    arrows: false,
-    speed: 300,
-    autoplay: false,
-    asNavFor: ".imgslider, .slick-wrap",
-  });
-  var imageSlide = $(".imgslider");
-  imageSlide.slick({
-    dots: false,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    vertical: true,
-    infinite: true,
-    arrows: false,
-    speed: 300,
-    autoplay: false,
-  });
+var slickWrap = $('.slick-wrap')
+slickWrap.slick({
+  dots: false,
+  autoplay: true,
+  //focusOnSelect: true,
+  infinite: true,
+  arrows: false,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  vertical: true,
+  //asNavFor: '.slidetxt',
+});
 
-  function jumpBack() {
-    setTimeout(function () {
+var slideTxt = $('.slidetxt')
+slideTxt.slick({
+  dots: false,
+  focusOnSelect: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  vertical: true,
+  infinite: false,
+  arrows: false,
+  speed: 300,
+  autoplay: true,
+  asNavFor: '.imgslider, .slick-wrap'
+})
+  var imageSlide = $('.imgslider')
+imageSlide.slick({
+  dots: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  vertical: true,
+  infinite: true,
+  arrows: false,
+  speed: 300,
+  autoplay: true,
+})
+
+
+function jumpBack() {
+    setTimeout(function() {
       slideTxt.slick("slickGoTo", 0);
       slickWrap.slick("slickGoTo", 0);
-    }, 1200);
+    }, 300);
   }
 
-  slideTxt.on("afterChange", function (event, slick, currentSlide, nextSlide) {
+  slideTxt.on("afterChange", function(event, slick, currentSlide, nextSlide) {
+    console.log(currentSlide - 1);
+    if (currentSlide === 3) {
+      console.log("last slide");
+      jumpBack();
+      //slideTxt.slick('slickPlay');
+    }
+  });
+  
+  imageSlide.on("afterChange", function(event, slick, currentSlide, nextSlide) {
     console.log(currentSlide - 1);
     if (currentSlide === 3) {
       console.log("last slide");
@@ -54,24 +65,13 @@
     }
   });
 
-  imageSlide.on(
-    "afterChange",
-    function (event, slick, currentSlide, nextSlide) {
-      console.log(currentSlide - 1);
-      if (currentSlide === 3) {
-        console.log("last slide");
-        jumpBack();
-        //slideTxt.slick('slickPlay');
-      }
-    }
-  );
-
-  slickWrap.on("afterChange", function (event, slick, currentSlide, nextSlide) {
+slickWrap.on("afterChange", function(event, slick, currentSlide, nextSlide) {
     console.log(currentSlide - 1);
     if (currentSlide === 3) {
       console.log("last slide");
       jumpBack();
-      slickWrap.slick("slickPlay");
+      slickWrap.slick('slickPlay');
     }
   });
+
 })(jQuery);
